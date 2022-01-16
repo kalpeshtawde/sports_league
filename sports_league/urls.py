d3a8b7efd31da0ed4e4fe5c898322fc0fbf60004
Kalpesh Tawde <kalpeshtawde@outlook.com>
2022-01-15 20:31:31 -0800

    modified urls

--- sports_league/urls.py
+++ sports_league/urls.py
@@ -8,12 +8,12 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+    2. Add a URL to urlpatterns:  path('', Homenf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.c.as_view(), name='home')
+Including another URLcoontrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from tennis.schema import schema
